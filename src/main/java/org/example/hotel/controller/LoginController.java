adc0991fc9ee8ef19cd4503e668c5ed38554a30e
Dulalet <daulet.abduali@gmail.com>
2020-11-15 23:00:36 +0600

    version before integrating admin

--- src/main/java/org/example/hotel/controller/LoginController.java
+++ src/main/java/org/example/hotel/controller/LoginController.java
@@ -30,31 +30,12 @@ public class LoginController {
         return "hello";
     }
 
-//    @GetMapping("/registration")
-//    public String registration() {
-//        return "registration";
-//    }
 
     @GetMapping("/registration")
     public String registration(){
         return "registration";
     }
 
-//    @PostMapping("/registration")
-//    public String addUser(User user, Model model) {
-//        User userFromDb = userRepo.findByUsername(user.getUsername());
-//
-//        if (userFromDb != null) {
-//            model.addAttribute("message", "User exists!");
-//            return "registration";
-//        }
-//
-//        user.setActive(true);
-//        user.setRoles(Collections.singleton(Role.USER));
-//        userRepo.save(user);
-//
-//        return "redirect:/login";
-//    }
 
     @PostMapping("registration")
     public String addUser(User user, Map<String, Object> model){
