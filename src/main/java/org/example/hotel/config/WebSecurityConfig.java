7da73b21f04b6103039abc7f07d4a0349dbf1c33
dulalet <daulet.abduali@gmail.com>
2020-11-15 22:17:57 +0600

    log/reg done

--- src/main/java/org/example/hotel/config/WebSecurityConfig.java
+++ src/main/java/org/example/hotel/config/WebSecurityConfig.java
@@ -18,16 +18,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .antMatchers("/", "/home", "/registration").permitAll()
-                .anyRequest().authenticated()
+                    .authorizeRequests()
+                    .antMatchers("/", "/home", "/registration").permitAll()
+                    .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .loginPage("/login")
-                .permitAll()
+                    .formLogin()
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/hello")
+                    .permitAll()
                 .and()
-                .logout()
-                .permitAll();
+                    .logout()
+                    .logoutSuccessUrl("/login")
+                    .permitAll();
     }
 
     @Override
